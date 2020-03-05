2fd8698a48e78a61d1cdd2afee141ef9b2c26130
pawan <pawanchoursiya50@gmail.com>
2020-03-06 02:57:03 +0530

    BMI

--- OOP Assignments/OOP Assignment 4/src/com/techlab/BMImodel/BMI.java
+++ OOP Assignments/OOP Assignment 4/src/com/techlab/BMImodel/BMI.java
@@ -1,45 +1,45 @@
 package com.techlab.BMImodel;
 
 public class BMI {
-	
+
 	double height, weight, BMI;
-	
-	public BMI(double weight, double height){
+
+	public BMI(double weight, double height) {
 		this.height = checkHeight(height);
 		this.weight = checkWeight(weight);
-		
+
 		calculateBMI();
 	}
 
 	private double checkHeight(double height) {
-		if(height <= 0) {
+		if (height <= 0) {
 			height = 1;
 		}
-		
-		return height/3.281;
+
+		return height / 3.281;
 	}
 
 	private double checkWeight(double weight) {
-		if(weight <= 0) {
+		if (weight <= 0) {
 			weight = 1;
 		}
 		return weight;
+
 	}
-	
-	private double calculateBMI(){
-		BMI =  (weight / (height*height));
+
+	private double calculateBMI() {
+		BMI = (weight / (height * height));
 		return BMI;
 	}
-	
+
 	public String catagory() {
-		if(BMI < 18.5) {
-			return "your BMI is = "+ BMI + " and you are underWeight";
+		if (BMI < 18.5) {
+			return "your BMI is = " + BMI + " and you are underWeight";
+		} else if (BMI > 24.9) {
+			return "your BMI is = " + BMI + " and you are overWeight";
 		}
-		else if(BMI > 24.9) {
-			return "your BMI is = "+ BMI + " and you are overWeight";
-		}
-		
-		return "your BMI is = "+ BMI + " and you are Normal";
+
+		return "your BMI is = " + BMI + " and you are Normal";
 	}
 
 }
