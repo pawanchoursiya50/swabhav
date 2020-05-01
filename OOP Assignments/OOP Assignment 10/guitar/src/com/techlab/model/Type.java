66260589cd66d6b19c77bed38dfe698e0be4fb5f
Pawan <pawanchoursiya50@gmail.com>
2020-05-01 20:37:40 +0530

    Assingment 10

---
+++ OOP Assignments/OOP Assignment 10/guitar/src/com/techlab/model/Type.java
@@ -0,0 +1,18 @@
+package com.techlab.model;
+
+public enum Type {
+	ACOUSTIC, ELECTRIC;
+
+	@Override
+	public String toString() {
+		switch (this) {
+		case ACOUSTIC:
+			return "Acoustic";
+		case ELECTRIC:
+			return "Electric";
+		default:
+			return null;
+		}
+	}
+
+}
